9492b6595ad9bb27e2639233ac0faab27fde10b5
Humauaca <carlosalejo.425@gmail.com>
2024-08-10 13:04:15 -0300

    djoser added

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -137,8 +137,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication'
     ),
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ]
 }
+
+DJOSER = {'USER_ID_FIELD': 'username'}
