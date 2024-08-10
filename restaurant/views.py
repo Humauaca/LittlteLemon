0680793c95ed5003c6f72161d83558ecb57dc14e
Humauaca <carlosalejo.425@gmail.com>
2024-08-10 12:28:50 -0300

    Authorization

--- restaurant/views.py
+++ restaurant/views.py
@@ -5,6 +5,7 @@ from . import models
 from . import serializers
 from rest_framework import generics
 from rest_framework import viewsets
+from rest_framework import permissions
 # Create your views here.
 
 def index(request):
@@ -13,6 +14,7 @@ def index(request):
 class BookingViewSet(viewsets.ModelViewSet):
     queryset = models.Booking.objects.all()
     serializer_class = serializers.BookingSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 
 class MenuItemView(generics.ListCreateAPIView):
